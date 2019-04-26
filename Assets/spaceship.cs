dbda7b6afd3ef462c60acaaf6c00f632566ca09c
Peredo <peredo98@hotmail.com>
2019-04-26 16:42:04 -0500

    spaceship movement

---
+++ Assets/spaceship.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spaceship : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody2D rb2d;
+
+
+    void Start()
+    {
+
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+      
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(0, moveVertical);
+
+        rb2d.velocity = (movement * speed);
+    }
+
+}
